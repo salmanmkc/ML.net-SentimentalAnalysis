fb323b0292cdcb46b2ee91c7181066242cfa38c3
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-05-27 12:07:50 +0100

    updated main app

--- myMLApp/Program.cs
+++ myMLApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MyMLAppML.Model.DataModels;
+using Microsoft.ML;
 
 namespace myMLApp
 {
@@ -6,7 +8,27 @@ namespace myMLApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsumeModel();
+        }
+
+        public static void ConsumeModel()
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.SentimentText = "Type your sentiment";
+
+            // Try model on sample data
+            // True is toxic, false is non-toxic
+            ModelOutput result = predEngine.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText} | Prediction: {(Convert.ToBoolean(result.Prediction) ? "Toxic" : "Non Toxic")} sentiment");
         }
     }
 }
